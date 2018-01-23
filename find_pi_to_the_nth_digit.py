85682ea089dd111c7dda99b68261144dacd2a27b
Matthias Diener <mat_die@uni-bremen.de>
2018-01-23 23:02:17 +0100

    find_pi_to_the_nth_digit

---
+++ find_pi_to_the_nth_digit.py
@@ -0,0 +1,12 @@
+import math
+
+pi = math.pi
+less_than_ten = "Please choose a number less than 10..."
+
+n = input("Give me a number...")
+
+if int(n) > 10:
+    print(less_than_ten)
+else:
+    format_string = '.' + n + 'f'
+    print(format(pi, format_string))
